3c7b880e665e80401248bc465eb204132bbe47ac
Erik Österlund <fisksvett@gmail.com>
2020-04-04 21:52:04 +0200

    forgot some things

---
+++ src/main/java/org.jpm/org/jpm/MySQLConfig.java
@@ -0,0 +1,8 @@
+package org.jpm;
+
+public class MySQLConfig {
+    public static final String _host = "localhost";
+    public static final String _database = "jpm";
+    public static final String _user = "jpm";
+    public static final String _password = "jpm";
+}
